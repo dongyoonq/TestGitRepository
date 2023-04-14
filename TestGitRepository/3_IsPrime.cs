07e714d7174ed6ea93f1c7469c296d0e128adbb3
동윤 김 <ut_community@naver.com>
2023-04-14 20:08:07 +0900

    feat : IsPrime 입력한 숫자가 소수인지 아닌지 판별하는 기능 추가

---
+++ TestGitRepository/3_IsPrime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGitRepository
+{
+    internal class IsPrime
+    {
+        public static bool CheckPrime(int n)
+        {
+            int cnt = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    cnt++;
+            }
+
+            return cnt == 2 ? true : false;
+        }
+    }
+}
