bd3ab064447288678b37bc6b2af93e4844971ea1
동윤 김 <ut_community@naver.com>
2023-04-14 20:12:24 +0900

    feat : SumOfDigits 사용자가 입력한 양의 정수의 각 자리수의 합을 구하는 기능

---
+++ TestGitRepository/4_SumOfDigits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGitRepository
+{
+    internal class SumOfDigits
+    {
+        public static int Run(int num)
+        {
+            int sum = 0;
+            while(num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
