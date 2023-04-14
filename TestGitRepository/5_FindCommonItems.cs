a6cddae93f8c69f6207f89d895af60c8ea670f36
동윤 김 <ut_community@naver.com>
2023-04-14 20:20:34 +0900

    feat : FindCommonItems k개의 정렬된 배열에서 공통항목을 찾는 기능 추가

---
+++ TestGitRepository/5_FindCommonItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestGitRepository
+{
+    internal class FindCommonItems
+    {
+        public static int[] Run(int[] arr1, int[] arr2, int[] arr3)
+        {
+            List<int> list = new List<int>();
+
+            for(int i = 0; i < arr1.Length; i++)
+                for(int j = 0; j < arr2.Length; j++)
+                    if(arr1[i] == arr2[j])
+                        list.Add(arr1[i]);
+
+            int[] temp = list.Distinct().ToArray();
+            list = new List<int>();
+
+            for(int i = 0; i < temp.Length;i++)
+                for(int j =0; j < arr3.Length; j++)
+                    if(temp[i] == arr3[j])
+                        list.Add(temp[i]);
+
+            temp = list.Distinct().ToArray();
+
+            return temp;
+        }
+    }
+}
